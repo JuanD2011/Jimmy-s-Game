b7dfbee683e7792b54c68445c472979d88dd869d
JuanD2011 <juan.atehortual@upb.edu.co>
2018-02-03 09:47:19 -0500

    Revert "Revert "Merge remote-tracking branch 'origin/Menu' into Develop""
    This reverts commit 3d1866bb518af294d15a448d33ab6d31895152ee.

---
+++ Jimmy's Game/Assets/Scripts/Menu/Menu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public string levelToLoad = " ";
+
+
+    public void Play()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+
+}
