acf459a60a702d3408e3cc1133caca9e3f605b65
JuanD2011 <juan.atehortual@upb.edu.co>
2018-02-04 15:17:01 -0500

    Jimmy Model, Row & Jump

---
+++ Jimmy's Game/Assets/Scripts/Jimmy/JimmyJump.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JimmyJump : MonoBehaviour
+{
+    Rigidbody mBody;
+    bool canJump;
+    public float jumpForce = 200f;
+
+	void Start ()
+    {
+        mBody = GetComponent<Rigidbody>();	
+	}
+	
+	void Update ()
+    {
+		if(canJump)
+        {
+            Jump();
+        }
+	}
+
+    void Jump()
+    {
+        if(Input.GetButtonDown("Jump"))
+        {
+            mBody.AddForce(transform.up * jumpForce);
+            mBody.AddForce(-transform.forward * 150f);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject collisioned = collision.gameObject;
+
+        if(collisioned.tag == "Water")
+        {
+            canJump = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        GameObject collisioned = collision.gameObject;
+
+        if(collisioned.tag == "Water")
+        {
+            canJump = false;
+        }
+    }
+}
