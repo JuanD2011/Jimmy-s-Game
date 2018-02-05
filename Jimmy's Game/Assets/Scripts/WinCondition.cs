930b1573725cf8ab894c79a52bbe9434d8fc5d33
danialarcon0698 <daniel.alarcon@upb.edu.co>
2018-02-04 22:40:11 -0500

    Water n leaderboard

---
+++ Jimmy's Game/Assets/Scripts/WinCondition.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinCondition : MonoBehaviour
+{
+    public float t = 0f;
+    public float time = 0f;
+
+    Dictionary<string, float> playerScores;
+
+    private void Start()
+    {
+        
+    }
+    private void Update()
+    {
+        t += Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject collisioned = other.gameObject;
+
+        if (collisioned.name == "P1")
+        {
+            time = t;
+        }
+    }
+}
