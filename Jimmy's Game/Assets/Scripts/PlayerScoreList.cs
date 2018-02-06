2b06a2e6fb5cade3f98e33c3f352963b00199b04
danialarcon0698 <daniel.alarcon@upb.edu.co>
2018-02-05 22:50:20 -0500

    Scoreboard

---
+++ Jimmy's Game/Assets/Scripts/PlayerScoreList.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScoreList : MonoBehaviour
+{
+    public GameObject playerScoreEntryPrefab;
+    ScoreManager scoreManager;
+
+    int lastChangeCounter;
+
+	void Start ()
+    {
+        scoreManager = GameObject.FindObjectOfType<ScoreManager>();
+
+        lastChangeCounter = scoreManager.GetChangeCounter();
+	}
+	
+	void Update ()
+    {
+        if (scoreManager == null)
+        {
+            Debug.LogError("You forgot to add the score manager component to a game object!");
+            return;
+        }
+
+        if (scoreManager.GetChangeCounter() == lastChangeCounter)
+        {
+            return;
+        }
+
+        lastChangeCounter = scoreManager.GetChangeCounter();
+
+        while (this.transform.childCount > 0)
+        {
+            Transform c = this.transform.GetChild(0);
+            c.SetParent(null); 
+
+            Destroy(c.gameObject);
+        }
+
+        string[] names = scoreManager.GetPlayerNames("Time");
+
+        foreach (string name in names)
+        {
+            GameObject go = (GameObject)Instantiate(playerScoreEntryPrefab);
+            go.transform.SetParent(this.transform);
+            go.transform.Find("TextUsername").GetComponent<Text>().text = name;
+            go.transform.Find("TextTime").GetComponent<Text>().text = scoreManager.GetScore(name, "Time").ToString();
+        }
+    }
+}
---
+++ Jimmy's Game/Assets/Scripts/RaceTime.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RaceTime : MonoBehaviour {
+
+    public Text timerText;
+    public GameObject colliderToStart;
+
+    public float timeCountDown = 3f;
+    public float raceTime = 0f;
+    public float sharedTime = 0f;
+    ScoreManager mScoreManager;
+
+    public GameObject scoreBoard;
+
+    public string playerOne = "P1";
+    public string playerTwo = "P2";
+    public string playerThree = "P3";
+    public string playerFive = "P5";
+    public string playerSix = "P6";
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        TimesUp();
+
+        raceTime += Time.deltaTime;
+
+    }
+
+    public void TimesUp()
+    {
+        if (timeCountDown < 4f && timeCountDown > -1f)
+        {
+            timeCountDown -= Time.deltaTime;
+
+            timerText.text = timeCountDown.ToString("0");
+        }
+        if (timeCountDown < 0)
+        {
+            timerText.gameObject.SetActive(false);
+
+            colliderToStart.gameObject.SetActive(false);
+        }
+    }
+
+    /*private void OnTriggerEnter(Collider other)
+    {
+        
+        GameObject collisioned = other.gameObject;
+
+        if (collisioned.name == playerOne)
+        {
+            sharedTime = raceTime;
+
+            mScoreManager.SetScore(playerOne, "Time", sharedTime);
+            i++;
+        }
+
+        if (collisioned.name == playerTwo)
+        {
+            sharedTime = raceTime;
+
+            mScoreManager.SetScore(playerTwo, "Time", sharedTime);
+            i++;
+        }
+
+        if (collisioned.name == playerThree)
+        {
+            sharedTime = raceTime;
+
+            mScoreManager.SetScore(playerThree, "Time", sharedTime);
+            i++;
+        }
+
+        if (collisioned.name == playerFive)
+        {
+            sharedTime = raceTime;
+
+            mScoreManager.SetScore(playerFive, "Time", sharedTime);
+            i++;
+        }
+        if (collisioned.name == playerSix)
+        {
+            sharedTime = raceTime;
+
+            mScoreManager.SetScore(playerSix, "Time", sharedTime);
+            i++;
+        }
+        if (collisioned.name == "JimmyIddle")
+        {
+            sharedTime = raceTime;
+            mScoreManager.SetScore("Jimmy", "Time", sharedTime);
+            i ++;
+        }
+    }*/
+}
